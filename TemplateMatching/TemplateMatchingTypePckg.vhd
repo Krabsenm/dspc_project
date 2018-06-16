d27c6da31bc6f444846ef3fbc69258cce51f2b4a
Jonas Kjær Rask <jonas.krask@gmail.com>
2018-06-16 09:48:24 +0200

    Extra stuff for window buffer

--- TemplateMatching/TemplateMatchingTypePckg.vhd
+++ TemplateMatching/TemplateMatchingTypePckg.vhd
@@ -12,6 +12,7 @@ package TemplateMatchingTypePckg is
   constant PIXEL_SIZE     : natural := 8;
   constant TEMPLATE_SIZE  : natural := 32;
   constant SCORE_MAX      : natural := TEMPLATE_SIZE*TEMPLATE_SIZE*(2**(PIXEL_SIZE));
+  constant NUM_SAD        : natural := 3;
   
   subtype Pixel_t  is unsigned(PIXEL_SIZE-1 downto 0);
   type ImageRow_t  is array(0 to IMAGE_WIDTH-1) of Pixel_t;
@@ -23,8 +24,18 @@ package TemplateMatchingTypePckg is
   subtype X_t      is integer range 0 to IMAGE_WIDTH-1;
   subtype Y_t      is integer range 0 to IMAGE_HEIGHT-1;
   
+  type WindowInfo_t is record
+    window : Window_t;
+    x      : X_t;
+    y      : Y_t;
+  end record;
   
-  constant Pixel_t_init : Pixel_t := (others => '0');
-  constant ImageRow_t_init : ImageRow_t := (others => Pixel_t_init);
+  type window_buffer_data_output_t is array(0 to NUM_SAD) of WindowInfo_t;
   
+  -- Reset types to all '0' constanst
+  constant Pixel_t_init      : Pixel_t      := (others => '0');
+  constant ImageRow_t_init   : ImageRow_t   := (others => Pixel_t_init);
+  constant WindowRow_t_init  : WindowRow_t  := (others => Pixel_t_init);
+  constant Window_t_init     : Window_t     := (others => WindowRow_t_init);
+  constant WindowInfo_t_init : WindowInfo_t := (window => Window_t_init, x => 0, y => 0);
 end package;
